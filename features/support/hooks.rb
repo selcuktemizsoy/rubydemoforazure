9a1943608b678bff2e360ff465a73df8309f9401
Selçuk Temizsoy <selcuk@kloia.com>
2021-08-12 14:40:16 +0300

    bug fixes

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -8,7 +8,7 @@ Before do |scenario|
 =end
   options.add_argument('--ignore-certificate-errors')
   options.add_argument('--no-sandbox')
-  options.add_arguments("--disable-dev-shm-usage");
+  options.add_argument("--disable-dev-shm-usage");
   Capybara.register_driver :selenium do |app|
     Capybara::Selenium::Driver.new(app,
                                    browser: :chrome,
